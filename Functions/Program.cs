55b1250d4e22ff517a92a09bd8a44248bfd2cfd2
Eric Faust <eric22f@gmail.com>
2024-07-19 15:50:59 -0700

    more changes to capture trace logs

--- Functions/Program.cs
+++ Functions/Program.cs
@@ -26,6 +26,7 @@ var host = new HostBuilder()
             logging.AddConsole();
             logging.AddApplicationInsights(); // Optional
             logging.AddConfiguration(configuration.GetSection("Logging"));
+            logging.SetMinimumLevel(LogLevel.Debug);
         });
 
         services.AddApplicationInsightsTelemetryWorkerService();
