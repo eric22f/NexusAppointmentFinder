8e7b25ae61131fb695500af4d792ae8ffe502b5b
Eric Faust <eric22f@gmail.com>
2024-07-24 17:58:15 -0700

    refactor tracer to ensure a unique id.

--- Functions/Helpers/Tracer.cs
+++ Functions/Helpers/Tracer.cs
@@ -7,26 +7,14 @@ namespace NexusAzureFunctions.Helpers;
 public class Tracer
 {
     private readonly string _traceId;
-    private readonly long _baseTicks = new DateTime(2024,7,1).Ticks;
 
     public Tracer()
     {
-        var ticks = DateTime.UtcNow.Ticks - _baseTicks;
-        string base36 = Base36Converter.ToBase36(ticks);
-        _traceId = InsertDashes(base36, 5);
+        _traceId = InsertDashes(Guid.NewGuid().ToString("N")[..9], 5);
     }
 
     // Unique Id for logging
     public string Id { get { return _traceId; } }
-
-    public DateTime GetDateTimeFromId(string id)
-    {
-        if (string.IsNullOrWhiteSpace(id))
-            throw new ArgumentException("Value cannot be null or whitespace.", nameof(id));
-
-        long ticks = long.Parse(Base36Converter.FromBase36(id.Replace("-", "")));
-        return new DateTime(ticks + _baseTicks);
-    }
 
     // Function to insert dashes after every n characters in a string
     private static string InsertDashes(string input, int n)
